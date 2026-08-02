f5dd57735eb84537f1bea891391a581b0ae8f906
G1M-Labor <connorhill1646@gmail.com>
2026-08-02 01:52:53 +0100

    Update burthorpemenuswapper.java

--- src/main/java/com/burthorpesellcalc/burthorpemenuswapper.java
+++ src/main/java/com/burthorpesellcalc/burthorpemenuswapper.java
@@ -2,18 +2,17 @@ package com.burthorpesellcalc;
 
 import com.google.inject.Inject;
 import net.runelite.api.Client;
-import net.runelite.api.MenuEntry;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
 
-public class BurthorpeMenuSwapper {
+public class burthorpemenuswapper {
     private final Client client;
-    private final BurthorpeCalcPlugin plugin;
+    private final burthorpecalcplugin plugin;
     private final ItemManager itemManager;
 
     @Inject
-    public BurthorpeMenuSwapper(Client client, BurthorpeCalcPlugin plugin, ItemManager itemManager) {
+    public burthorpemenuswapper(Client client, burthorpecalcplugin plugin, ItemManager itemManager) {
         this.client = client;
         this.plugin = plugin;
         this.itemManager = itemManager;
@@ -25,6 +24,5 @@ public class BurthorpeMenuSwapper {
         if (option == null || !option.contains("Set (Sell -")) {
             return;
         }
-        // Menu item customization event callbacks routed cleanly here
     }
 }
