ec32479f53324752f4b1bc8ad2359d12fdca3125
G1M-Labor <connorhill1646@gmail.com>
2026-08-02 07:11:57 +0100

    fixed bugs burthorpeitemoverlay.java
    working on test environment

--- src/main/java/com/burthorpesellcalc/burthorpeitemoverlay.java
+++ src/main/java/com/burthorpesellcalc/burthorpeitemoverlay.java
@@ -1,13 +1,14 @@
 package com.burthorpesellcalc;
 
-import com.google.inject.Inject;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import net.runelite.api.Client;
-import net.runelite.api.widgets.WidgetItem;
+import net.runelite.api.ItemContainer;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class burthorpeitemoverlay extends WidgetItemOverlay {
     private final Client client;
@@ -26,27 +27,72 @@ public class burthorpeitemoverlay extends WidgetItemOverlay {
     }
 
     @Override
-    public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem itemWidget) {
+    public void renderItemOverlay(Graphics2D graphics, int itemId, net.runelite.api.widgets.WidgetItem widgetItem) {
         int canonicalId = itemManager.canonicalize(itemId);
         if (!plugin.isItemIncluded(canonicalId)) {
             return;
         }
 
-        boolean isBank = itemWidget.getWidget().getParent().getId() == 786445;
-        if (isBank && !config.enableBankHighlight()) {
+        int packedWidgetId = widgetItem.getWidget().getId();
+        int widgetGroupId = packedWidgetId >> 16;
+        boolean isBankWidget = (widgetGroupId == 12);
+
+        if (isBankWidget && !config.enableBankHighlight()) {
             return;
         }
-        if (!isBank && !config.enableInventoryHighlight()) {
+        if (!isBankWidget && !config.enableInventoryHighlight()) {
             return;
         }
 
-        // FIXED INTERFACE CALL: Swapped out broken .getBounds() for the required .getCanvasBounds() method parameter loop
-        Rectangle bounds = itemWidget.getCanvasBounds();
-        if (bounds == null) {
+        int batchSize = 1;
+        Integer forcedBatch = plugin.getForcedBatchSize(canonicalId);
+        if (forcedBatch != null) {
+            batchSize = forcedBatch;
+        }
+
+        if (!isBankWidget && batchSize != burthorpecalcplugin.SELL_AMOUNT_ALL) {
+            Widget shopGrid = client.getWidget(301, 16);
+            if (shopGrid == null || shopGrid.isHidden()) {
+                shopGrid = client.getWidget(300, 16);
+            }
+
+            if (shopGrid != null && !shopGrid.isHidden()) {
+                Widget[] shopItems = shopGrid.getDynamicChildren();
+                if (shopItems != null) {
+                    for (Widget shopItemWidget : shopItems) {
+                        if (shopItemWidget != null && itemManager.canonicalize(shopItemWidget.getItemId()) == canonicalId) {
+                            if (shopItemWidget.getItemQuantity() > 0) {
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int containerId = isBankWidget ? 95 : 93;
+        ItemContainer container = client.getItemContainer(containerId);
+        if (container == null) {
             return;
         }
 
-        graphics.setColor(new Color(255, 152, 31, 180));
-        graphics.drawRect(bounds.x, bounds.y, bounds.width - 1, bounds.height - 1);
+        Color strokeColor;
+        // FIX: Evaluates explicit structural equality ahead of standard switch fallbacks
+        if (batchSize == burthorpecalcplugin.SELL_AMOUNT_ALL) {
+            strokeColor = Color.RED;
+        } else {
+            switch (batchSize) {
+                case 1: strokeColor = Color.CYAN; break;
+                case 5: strokeColor = Color.GREEN; break;
+                case 10: strokeColor = Color.YELLOW; break;
+                case 50: strokeColor = Color.ORANGE; break;
+                default: strokeColor = Color.GREEN; break;
+            }
+        }
+
+        BufferedImage itemOutlineImage = itemManager.getItemOutline(itemId, widgetItem.getQuantity(), strokeColor);
+        if (itemOutlineImage != null) {
+            graphics.drawImage(itemOutlineImage, widgetItem.getCanvasLocation().getX(), widgetItem.getCanvasLocation().getY(), null);
+        }
     }
 }
