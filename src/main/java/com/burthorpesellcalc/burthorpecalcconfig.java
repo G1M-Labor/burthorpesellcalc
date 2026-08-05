f8a3f723cbb0e93a5002665275c83946e6686044
G1M-Labor <connorhill1646@gmail.com>
2026-08-05 03:41:02 +0100

    Update burthorpecalcconfig.java
    Added instructions for Bank tag integration

--- src/main/java/com/burthorpesellcalc/burthorpecalcconfig.java
+++ src/main/java/com/burthorpesellcalc/burthorpecalcconfig.java
@@ -3,9 +3,29 @@ package com.burthorpesellcalc;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("burthorpesellcalc")
 public interface burthorpecalcconfig extends Config {
+
+    @ConfigSection(
+            name = "Bank Tag Integration Info",
+            description = "Information regarding Bank Tag integration. Right-click, reset this 'Bank Tag Guide' to reset infobox.",
+            position = 0,
+            closedByDefault = false
+    )
+    String integrationSection = "integrationSection";
+
+    @ConfigItem(
+            keyName = "tagNoticeText",
+            name = "Bank Tag Guide",
+            description = "",
+            position = 1,
+            section = "integrationSection"
+    )
+    default String tagNoticeBox() {
+        return "Selected items are automatically imported to the 'shopscape' bank tag. Please create this tag in-game to ensure easy item access. Warning: excluding items in layout mode requires the layout to be enabled and disabled to apply.";
+    }
 
     enum ValueFormatMode {
         ROUNDED("Rounded"),
@@ -26,26 +46,26 @@ public interface burthorpecalcconfig extends Config {
     @ConfigItem(
             keyName = "valueFormat",
             name = "Value Format",
-            description = "Decide how gp values show within shift rightclick and your bank header",
-            position = 1
+            description = "Decide how gp values show within shift right-click and your bank header",
+            position = 2
     )
     default ValueFormatMode valueFormat() {
         return ValueFormatMode.ROUNDED;
     }
 
-    @ConfigItem(keyName = "shiftBankMenu", name = "Shift Bank Menu", description = "", position = 2)
+    @ConfigItem(keyName = "shiftBankMenu", name = "Shift Right-click : Bank Menu", description = "Toggles Shift right-click options within the Bank", position = 3)
     default boolean shiftBankMenu() { return true; }
 
-    @ConfigItem(keyName = "shiftInventoryMenu", name = "Shift Inventory Menu", description = "", position = 3)
+    @ConfigItem(keyName = "shiftInventoryMenu", name = "Shift Right-click : Inventory Menu", description = "Toggles Shift right-click options within the inventory", position = 4)
     default boolean shiftInventoryMenu() { return true; }
 
-    @ConfigItem(keyName = "enableBankHighlight", name = "Enable Bank Highlight", description = "", position = 4)
+    @ConfigItem(keyName = "enableBankHighlight", name = "Enable Bank Highlight", description = "Toggles highlighting included items within the bank", position = 5)
     default boolean enableBankHighlight() { return true; }
 
-    @ConfigItem(keyName = "enableInventoryHighlight", name = "Enable Inventory Highlight", description = "", position = 5)
+    @ConfigItem(keyName = "enableInventoryHighlight", name = "Enable Shop Highlight", description = "Toggles highlighting included items within the shop", position = 6)
     default boolean enableInventoryHighlight() { return true; }
 
-    @ConfigItem(keyName = "clearInclusionsToggle", name = "Clear Database", description = "", position = 6)
+    @ConfigItem(keyName = "clearInclusionsToggle", name = "Clear Database", description = "Clear all included items and their sell quantities", position = 7)
     default boolean clearInclusionsToggle() { return false; }
 
     @ConfigItem(keyName = "includedItemIds", name = "", description = "", hidden = true)
